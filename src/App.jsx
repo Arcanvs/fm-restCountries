402de77f9b6d1ce8afa1cf7980fa9318634d6520
Jose <jose.osorio.herrera@gmail.com>
2023-01-13 00:57:38 -0300

    [UPD] config rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import CountriesTemplate from './components/CountriesTemplate';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar'
+import { HomePage, CountryPage } from './components/pages';
 
 function App() {
   return (
     <div className="App">
       <div className="countries">
         <Navbar />
-        <CountriesTemplate />
+        <div className="countries__body">
+          <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/country/:id' element={<CountryPage />} />
+                <Route
+                    path="*"
+                    element={<Navigate to="/" replace />}
+                />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
     </div>
   )
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Card from './Card'
-import Filter from './Filter'
+import Card from '../Card'
+import Filter from '../Filter'
 
 const HomePage = () => {
   return (
